dc747c45fbda0959a05c2401bff271bcc8b31f29
Sam Billingham <sambillingham@Sams-MacBook-Air.local>
2022-03-13 18:12:11 +0100

    update GM Meerch Contract

--- contracts/GMMerch.sol
+++ contracts/GMMerch.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
+
+// ▒█▀▀█ ▒█▀▀▀█ ▒█▀▀▀█ ▒█▀▀▄ 　 ▒█▀▄▀█ ▒█▀▀▀█ ▒█▄░▒█ ▒█░▄▀ ▒█▀▀▀ ▒█░░▒█ ▒█▀▀▀█ 　 ▒█▀▄▀█ ▒█▀▀▀ ▒█▀▀█ ▒█▀▀█ ▒█░▒█ 
+// ▒█░▄▄ ▒█░░▒█ ▒█░░▒█ ▒█░▒█ 　 ▒█▒█▒█ ▒█░░▒█ ▒█▒█▒█ ▒█▀▄░ ▒█▀▀▀ ▒█▄▄▄█ ░▄▄▄▀▀ 　 ▒█▒█▒█ ▒█▀▀▀ ▒█▄▄▀ ▒█░░░ ▒█▀▀█ 
+// ▒█▄▄█ ▒█▄▄▄█ ▒█▄▄▄█ ▒█▄▄▀ 　 ▒█░░▒█ ▒█▄▄▄█ ▒█░░▀█ ▒█░▒█ ▒█▄▄▄ ░░▒█░░ ▒█▄▄▄█ 　 ▒█░░▒█ ▒█▄▄▄ ▒█░▒█ ▒█▄▄█ ▒█░▒█
+
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "hardhat/console.sol";
 
 contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
     constructor()
-        ERC1155("https://ipfs.io/ipfs/QmPDLZhhtFkNp94PYpa63A5sKPax4dog9K3So1PUjoq9pf/{id}.json")
+        ERC1155("https://ipfs.io/ipfs/QmcAU59G6omEg6kbNyP1Rz8R9zevS8owfXcsBpFoNQFVo9/{id}.json")
         {}
     
     mapping(address => mapping(uint256 => bool)) public mintList;
@@ -30,7 +34,12 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
     }
     MerchItem[] public merch;
 
-    function createMerchItem(uint256 _supply, uint256 _price, uint256 _increment) public onlyOwner{
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+    
+    function createMerchItem(uint256 _supply, uint256 _price, uint256 _increment) public onlyOwner {
         merch.push(MerchItem({
             minted: 0,
             burned: 0,
@@ -44,7 +53,7 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
         }));
     }
 
-    function updateMerchItem(uint _index, uint256 _supply, uint256 _price, uint256 _increment, uint256 _incrementInterval, bool _allowMintable, bool _publicMintable, bool _mintPassIncluded) public {
+    function updateMerchItem(uint _index, uint256 _supply, uint256 _price, uint256 _increment, uint256 _incrementInterval, bool _allowMintable, bool _publicMintable, bool _mintPassIncluded) public onlyOwner {
         MerchItem storage m = merch[_index];
         m.supply = _supply;
         m.price = _price;
@@ -57,10 +66,6 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
 
     function getMerchCount() public view returns(uint merchCount) {
         return merch.length;
-    }
-
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
     }
 
     function recoverSigner(address _address, bytes memory signature, uint256 id) public pure returns (address) {
@@ -76,7 +81,7 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
     function mintToken(uint256 id) public payable {
         require(merch[id].publicMintable , "MINTING - ALLOW ONLY");
         require(msg.value >= merch[id].price, "Not enough ETH sent");
-        require(merch[id].minted <= merch[id].supply, "Sold out");
+        require(merch[id].minted < merch[id].supply, "Sold out");
         require(!mintList[msg.sender][id], "ONLY 1 MINT PER WALLET");
 
         _mint(msg.sender, id, 1, '');
@@ -97,8 +102,7 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
     function mintTokenAllow(uint256 id, bytes memory signature) public payable {
         require(merch[id].allowMintable , "MINTING - NOT OPEN");
         require(msg.value >= merch[id].price, "Not enough ETH sent");
-        require(merch[id].minted <= merch[id].supply, "Sold out");
-        
+        require(merch[id].minted < merch[id].supply, "Sold out");
         require(recoverSigner(msg.sender, signature, id) == owner(), "Address is not allowlisted");
         require(!mintList[msg.sender][id], "ONLY 1 MINT PER WALLET");
 
@@ -112,6 +116,7 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
         }
 
         if(merch[id].mintPassIncluded) {
+            merch[1].minted += 1;
             _mint(msg.sender, 1, 1, '');
         }
         emit GMMinted(msg.sender, id);
@@ -119,7 +124,7 @@ contract GMMerch is ERC1155, Ownable, ERC1155Burnable {
     
     function mintAdmin(address to, uint256 id, uint256 amount) public onlyOwner {
         _mint(to, id, amount, '');
-        merch[id].minted += 1;
+        merch[id].minted += amount;
     }
 
     function burnToken(address _address, uint256 id) public {
